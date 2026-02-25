9020c15404e28e22d439950e6e8c4e216ce8d4f3
Tim Ding <hd840@nyu.edu>
2026-02-24 22:01:39 -0700

    add scenario runner

---
+++ scenario_runner_modified/carla_env_37.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export PYTHONPATH=/home/sim273/CARLA/PythonAPI/carla/dist/carla-0.9.15-py3.7-linux-x86_64.egg:/home/sim273/CARLA/PythonAPI/carla
+echo "Loaded CARLA environment for Python 3.7"
